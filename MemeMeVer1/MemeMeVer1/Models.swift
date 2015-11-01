86a37129cac41d2c306746300fd985f5f9186159
qwertyshan <rao.shantanu@gmail.com>
2015-11-01 01:12:39 -0800

    Meme App ver 1

---
+++ MemeMeVer1/MemeMeVer1/Models.swift
@@ -0,0 +1,17 @@
+//
+//  meme.swift
+//  MemeMeVer1
+//
+//  Created by Shantanu Rao on 10/30/15.
+//  Copyright © 2015 Shantanu Rao. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var image: UIImage
+    var meme: UIImage
+}
